e6394cbf43cf12dbd8919bf6ae5f3c931a9a1211
Miidoriya <99621562+Miidoriya@users.noreply.github.com>
2022-10-02 15:02:38 +0100

    [typescript-node-setup] Switched to aws-sdk/client-pricing sdk. WIP for EC2 instance price retrieval (Current not historic)

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,36 @@
-import { GetProductsCommand, GetProductsCommandOutput, GetProductsRequest, PricingClient } from "@aws-sdk/client-pricing";
+import {
+  GetProductsCommand,
+  GetProductsCommandOutput,
+  GetProductsRequest,
+  PricingClient,
+} from "@aws-sdk/client-pricing";
 
 const client = new PricingClient({ region: "us-east-1" });
-const fs = require('fs')
+const fs = require("fs");
 
 const params: GetProductsRequest = {
-    ServiceCode: "AmazonEC2"
+  ServiceCode: "AmazonEC2",
 };
 const command = new GetProductsCommand(params);
 
-
 const getPrices = async () => {
-    console.log("Console logging DescribeServicesCommand client.send")
+  console.log("Console logging DescribeServicesCommand client.send");
   try {
     const data: GetProductsCommandOutput = await client.send(command);
     if (data.PriceList) {
-        data.PriceList.forEach(item => {
-            console.log(JSON.parse(item.toString()))
-        })
-        fs.writeFile('Output.txt', data.PriceList.toString(), (err: Error) => {
-      
-            // In case of a error throw err.
-            if (err) throw err;
-        })
+      data.PriceList.forEach((item) => {
+        console.log(JSON.parse(item.toString()));
+      });
+      fs.writeFile("Output.txt", data.PriceList.toString(), (err: Error) => {
+        // In case of a error throw err.
+        if (err) throw err;
+      });
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
 (async () => {
-    await getPrices();
+  await getPrices();
 })();
