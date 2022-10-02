c06575e7c90e66a5a3eca0ff4bbf3ed15198a66a
Miidoriya <99621562+Miidoriya@users.noreply.github.com>
2022-10-02 13:12:57 +0100

    Added prettier linting

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,30 @@
-import { OfferIndex } from "./interfaces/offer-index"
+import { OfferIndex } from "./interfaces/offer-index";
 
-console.log("AWS Price API POC!")
-const offerIndexURL = 'https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json'
+console.log("AWS Price API POC!");
+const offerIndexURL =
+  "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json";
 
 const getOfferIndexFile = async (): Promise<OfferIndex> => {
-    const response = await fetch(offerIndexURL)
-    return await response.json()
-}
+  const response = await fetch(offerIndexURL);
+  return await response.json();
+};
 
 const logOfferIndexFile = async () => {
-    try {
-        const text = await getOfferIndexFile();
-        console.log(text)
-    } catch (e) {
-        console.log("Error retrieving Index File")
-        console.error(e)
-    }
-}
+  try {
+    const text = await getOfferIndexFile();
+    console.log(text);
+  } catch (e) {
+    console.log("Error retrieving Index File");
+    console.error(e);
+  }
+};
 
 const getOfferDetailsForService = async (service: string) => {
-    console.log(`Getting Offer details for service ... ${service}`)
-    const file = await getOfferIndexFile();
-    console.log(file.offers[service])
-}
+  console.log(`Getting Offer details for service ... ${service}`);
+  const file = await getOfferIndexFile();
+  console.log(file.offers[service]);
+};
 
-(async () => { 
-    await getOfferDetailsForService("AmazonLightsail") 
+(async () => {
+  await getOfferDetailsForService("AmazonLightsail");
 })();
