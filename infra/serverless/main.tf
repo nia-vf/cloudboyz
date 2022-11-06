b5fbac7e21beb6dc88a4af0797e1679c881b754b
nia-vf <nia_vf@hotmail.com>
2022-11-06 15:13:27 +0000

    line 11 - added terraform fmt to lint-fix command

--- infra/serverless/main.tf
+++ infra/serverless/main.tf
@@ -32,10 +32,10 @@ module "lambda_function" {
 
 #Lambda IAM Permissions for API Gateway
 resource "aws_lambda_permission" "pricing_agw_perm" {
-  statement_id = "AllowExecutionFromAPIGateway"
-  action = "lambda:InvokeFunction"
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
   function_name = module.lambda_function.lambda_function_name
-  principal = "apigateway.amazonaws.com"
+  principal     = "apigateway.amazonaws.com"
 
   source_arn = "arn:aws:execute-api:${var.my_region}:${var.account_id}:${aws_api_gateway_rest_api.pricing_agw.id}/*/${aws_api_gateway_method.get_method.http_method}${aws_api_gateway_resource.ec2_pricing.path}"
 }
@@ -50,30 +50,30 @@ resource "aws_api_gateway_rest_api" "pricing_agw" {
 }
 
 resource "aws_api_gateway_resource" "ec2_pricing" {
-  parent_id = aws_api_gateway_rest_api.pricing_agw.root_resource_id
+  parent_id   = aws_api_gateway_rest_api.pricing_agw.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.pricing_agw.id
-  path_part = "ec2-pricing"
+  path_part   = "ec2-pricing"
 }
 
 #API Gateway GET Resource
 resource "aws_api_gateway_method" "get_method" {
-  resource_id = aws_api_gateway_resource.ec2_pricing.id
-  rest_api_id = aws_api_gateway_rest_api.pricing_agw.id
+  resource_id   = aws_api_gateway_resource.ec2_pricing.id
+  rest_api_id   = aws_api_gateway_rest_api.pricing_agw.id
   authorization = "NONE"
-  http_method = "GET"
+  http_method   = "GET"
   request_parameters = {
     "method.request.querystring.instanceType" = true
-    "method.request.querystring.region" = true
+    "method.request.querystring.region"       = true
   }
 }
 
 resource "aws_api_gateway_integration" "get_integration" {
-  resource_id = aws_api_gateway_resource.ec2_pricing.id
-  rest_api_id = aws_api_gateway_rest_api.pricing_agw.id
-  http_method = aws_api_gateway_method.get_method.http_method
+  resource_id             = aws_api_gateway_resource.ec2_pricing.id
+  rest_api_id             = aws_api_gateway_rest_api.pricing_agw.id
+  http_method             = aws_api_gateway_method.get_method.http_method
   integration_http_method = "POST"
-  type = "AWS"
-  uri = module.lambda_function.lambda_function_invoke_arn
+  type                    = "AWS"
+  uri                     = module.lambda_function.lambda_function_invoke_arn
 
   request_templates = {
     "application/json" = <<MAPPING_TEMPLATE
@@ -123,17 +123,17 @@ resource "aws_api_gateway_integration_response" "get_int_response_200" {
 
 #API Gateway OPTIONS Resource
 resource "aws_api_gateway_method" "opt_method" {
-  resource_id = aws_api_gateway_resource.ec2_pricing.id
-  rest_api_id = aws_api_gateway_rest_api.pricing_agw.id
+  resource_id   = aws_api_gateway_resource.ec2_pricing.id
+  rest_api_id   = aws_api_gateway_rest_api.pricing_agw.id
   authorization = "NONE"
-  http_method = "OPTIONS"
+  http_method   = "OPTIONS"
 }
 
 resource "aws_api_gateway_integration" "opt_integration" {
   resource_id = aws_api_gateway_resource.ec2_pricing.id
   rest_api_id = aws_api_gateway_rest_api.pricing_agw.id
   http_method = aws_api_gateway_method.opt_method.http_method
-  type = "MOCK"
+  type        = "MOCK"
 
   request_templates = {
     "application/json" = <<MAPPING_TEMPLATE
@@ -209,6 +209,6 @@ resource "aws_api_gateway_deployment" "pricing_agw_deploy" {
 
 resource "aws_api_gateway_stage" "pricing_agw_stage" {
   deployment_id = aws_api_gateway_deployment.pricing_agw_deploy.id
-  rest_api_id = aws_api_gateway_rest_api.pricing_agw.id
-  stage_name = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.pricing_agw.id
+  stage_name    = "prod"
 }
--- infra/serverless/providers.tf
+++ infra/serverless/providers.tf
@@ -10,5 +10,5 @@ terraform {
 }
 
 provider "aws" {
-  region  = "eu-west-2"
+  region = "eu-west-2"
 }
