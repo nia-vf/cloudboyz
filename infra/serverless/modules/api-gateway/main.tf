ac45a6864a52222d9d590dd39b1a9a1fed95d67c
nia-vf <nia_vf@hotmail.com>
2022-11-12 12:14:10 +0000

    changes to api-gateway module

--- infra/serverless/modules/api-gateway/main.tf
+++ infra/serverless/modules/api-gateway/main.tf
@@ -1,4 +1,14 @@
-#API Gateway
+#API Gateway REST API
 resource "aws_api_gateway_rest_api" "api_gateway" {
   name = var.name
 }
+
+#API Fateawy Resource
+resource "aws_api_gateway_resource" "api_gateway_resource" {
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  path_part   = var.resource_name
+}
+
+
+
--- infra/serverless/modules/api-gateway/variables.tf
+++ infra/serverless/modules/api-gateway/variables.tf
@@ -2,3 +2,21 @@ variable "name" {
   description = "Name of the api-gateway to create"
   type        = string
 }
+
+variable "resource_name" {
+  description = "Name of the API resource, also defined as the path part"
+  type = string
+  default = "ec2-pricing"
+}
+
+#API Gateway GET Resource
+resource "aws_api_gateway_method" "get_method" {
+  resource_id   = aws_api_gateway_resource.api_gateway_resource.id
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  authorization = "NONE"
+  http_method   = "GET"
+  request_parameters = {
+    "method.request.querystring.instanceType" = true
+    "method.request.querystring.region"       = true
+  }
+}
