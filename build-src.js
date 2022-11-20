dab1eedf4606550820535460af5775553d89490f
nia-vf <nia_vf@hotmail.com>
2022-11-20 12:56:07 +0000

    added comments and console.logs to build-src.js

--- build-src.js
+++ build-src.js
@@ -3,7 +3,7 @@ var admZip = require("adm-zip");
 var fs = require("fs");
 var path = require("path");
 
-const rootDir = __dirname.replace(/\\/g, "/");
+const rootDir = __dirname.replace(/\\/g, "/"); //where we see 'replace(/\\/g, "/")' this is to handle paths using backslashes rather than forward slashes
 const srcDirPath = rootDir + "/src/lambdas";
 const distDirPath = rootDir + "/dist";
 
@@ -21,21 +21,24 @@ function getPaths(dirPath, arrayFiles) {
   return arrayFiles;
 }
 
+//Get list of Services
 function getServices(paths, dirPath) {
   let services = [];
   paths.forEach((servicePath) => {
-    dirPath = dirPath.replace(/\//g, "\\");
+    dirPath = dirPath.replace(/\//g, "\\"); //change forward-slash to backslash
     servicePath = servicePath.split(dirPath)[1];
     services.push(servicePath);
   });
+  console.log("List of existing services: ", services);
   return services;
 }
 
+//Bundle handler.ts files and output them in dist directory as index.js files
+//This is a recursive function to identify ALL handler.ts files in our src/lambdas directory
 function bundleAppCode(services, esBuild) {
   var dists = [];
   services.forEach((service) => {
     servicePath = srcDirPath + service.replace(/\\/g, "/");
-    //console.log(servicePath)
     serviceName = service.replace(/\\/g, "/").split("/handler.ts")[0];
     esBuild
       .build({
@@ -49,32 +52,37 @@ function bundleAppCode(services, esBuild) {
       })
       .catch((error) => process.exit(1));
     dists.push(rootDir + "/dist" + serviceName + "/index.js");
-    console.log("'" + rootDir + "/dist" + serviceName + "/index.js'created.");
+    console.log("'" + rootDir + "/dist" + serviceName + "/index.js' created.");
   });
+  console.log("List of created dist index.js files: ", dists);
   return dists;
 }
 
+//Wait 5 seconds - to allow for new dist directories to become available
 function sleep(ms) {
+  console.log("Waiting 5 seconds");
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+//Zip index.js files into index.zip files
 async function zipAppCode(esBuildOutput) {
   await sleep(5000);
   console.log("Waited 5 seconds");
 
+  var zipList = [];
   esBuildOutput.forEach((file) => {
     var data = fs.readFileSync(file);
-    //var content = "what a pile of rubbbish"
     var content = data;
     var zip = new admZip();
     zip.addFile("index.js", Buffer.from(content, "utf8"));
     zip.writeZip(file.split("/index.js")[0] + "/index.zip");
     console.log("index.zip written to '" + file.split("/index.js")[0] + "'");
+    zipList.push(file.split("/index.js")[0] + "/index.zip");
   });
+  console.log("List of created dist index.zip files: ", zipList);
 }
 
 var folders = getPaths(srcDirPath);
 var services = getServices(folders, srcDirPath);
 var esBuildOutput = bundleAppCode(services, esBuild);
-console.log(esBuildOutput);
 zipAppCode(esBuildOutput);
