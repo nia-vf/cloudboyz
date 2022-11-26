8b93ba4e82275640c02c06bc6ac192417dbde65c
Miidoriya <99621562+Miidoriya@users.noreply.github.com>
2022-11-26 17:48:25 +0000

    Comments

--- scripts/build-src.ts
+++ scripts/build-src.ts
@@ -4,10 +4,10 @@ import fs from "fs";
 import esBuild from "esbuild";
 
 /**
-* Function that replaces file path backslashes with forward slashes (Resolves windows usage)
-* @param    {String} path Path containing backslashes
-* @return   {String}      Formatted path
-*/
+ * Function that replaces file path backslashes with forward slashes (Resolves windows usage)
+ * @param    {String} path Path containing backslashes
+ * @return   {String}      Formatted path
+ */
 const windowsReplace = (path: string): string => {
   return path.replace(/\\/g, "/");
 };
@@ -18,11 +18,11 @@ const srcDirPath = rootDir + "/src/lambdas";
 type HandlerFilePaths = string[];
 
 /**
-* Function that gets a list of `full paths` to lambda handler.ts files
-* @param    {String} srcDirPath Source directory to recursively explore for handler.ts files
-* @param    {String} handlerFilePaths Array of paths to lambda handler.ts files. Passed back into the function recursively. Undefined by default
-* @return   {String}            Array of paths to lambda handler.ts files
-*/
+ * Function that gets a list of `full paths` to lambda handler.ts files
+ * @param    {String} srcDirPath Source directory to recursively explore for handler.ts files
+ * @param    {String} handlerFilePaths Array of paths to lambda handler.ts files. Passed back into the function recursively. Undefined by default
+ * @return   {String}            Array of paths to lambda handler.ts files
+ */
 const getPaths = (
   srcDirPath: string,
   handlerFilePaths?: HandlerFilePaths
@@ -41,21 +41,21 @@ const getPaths = (
 };
 
 /**
-* Function to parse the lambda name from a path string
-* 
-* @param    {String} path Path string containing lambda name
-* @return   {String}      Lambda name string. E.g. `hello-world`, `hello-world/simplified`
-*/
+ * Function to parse the lambda name from a path string
+ *
+ * @param    {String} path Path string containing lambda name
+ * @return   {String}      Lambda name string. E.g. `hello-world`, `hello-world/simplified`
+ */
 const getLambdaName = (path: string): string => {
   const lambdaPath = windowsReplace(path).replace(srcDirPath, "");
   return `${lambdaPath.split("/handler.ts")[0]}`;
 };
 
 /**
-* Function that bundles provided typescript handler paths into minimized .js files and moves to distinct dist folder locations
-* @param    {String} lambdaPaths Array of paths to handler.ts files
-* @return   {String}             List of paths to bundles.js files in the dist folder
-*/
+ * Function that bundles provided typescript handler paths into minimized .js files and moves to distinct dist folder locations
+ * @param    {String} lambdaPaths Array of paths to handler.ts files
+ * @return   {String}             List of paths to bundles.js files in the dist folder
+ */
 const bundleAppCode = (lambdaPaths: string[]): string[] => {
   const dists: string[] = [];
   lambdaPaths.forEach((lambdaFullPath: string) => {
@@ -82,9 +82,9 @@ const bundleAppCode = (lambdaPaths: string[]): string[] => {
 };
 
 /**
-* Function that pauses execution of a script
-* @param    {String} ms Amount of time in milliseconds to pause execution
-*/
+ * Function that pauses execution of a script
+ * @param    {String} ms Amount of time in milliseconds to pause execution
+ */
 const sleep = async (ms: number) => {
   const waitTimeInSeconds = ms / 1000;
   console.log(`Waiting ${waitTimeInSeconds} seconds`);
@@ -92,9 +92,9 @@ const sleep = async (ms: number) => {
 };
 
 /**
-* Function that zips code in the provided path list into .zip files
-* @param    {String} pathsToZip Paths containing .js code to zip
-*/
+ * Function that zips code in the provided path list into .zip files
+ * @param    {String} pathsToZip Paths containing .js code to zip
+ */
 const zipAppCode = async (pathsToZip: string[]) => {
   await sleep(3000);
   console.log(
@@ -120,6 +120,6 @@ const main = () => {
   console.log("List of existing lambda paths (handler.ts): ", lambdaPaths);
   const esBuildOutput = bundleAppCode(lambdaPaths);
   zipAppCode(esBuildOutput);
-}
+};
 
-main()
+main();
