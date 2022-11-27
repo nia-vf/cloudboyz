04f94e93106efda1f1ae609895e3d00f69c7e6ca
nia-vf <nia_vf@hotmail.com>
2022-11-27 21:17:43 +0000

    added env file which is used by ResourceSkus.ts file

--- src/services/azure/compute/ResourceSkus.ts
+++ src/services/azure/compute/ResourceSkus.ts
@@ -1,8 +1,20 @@
+import { ClientSecretCredential, DefaultAzureCredential } from "@azure/identity";
+import * as dotenv from "dotenv";
 import { ResourceSkus } from "@azure/arm-compute";
-import { getProducts } from "./function/ListResourceSkus";
+//import { getProducts } from "./function/ListResourceSkus";
 
-export class ResourceSkusAPI {
-  getProducts(filtersMap: Map<string, string>): Promise<ResourceSkus[]> {
-    return getProducts(filtersMap);
-  }
-}
+dotenv.config({ path: __dirname + "/./../../../../.env" } );
+
+let credentials = null;
+
+const tenantId = process.env["AZURE_TENANT_ID"]
+console.log(tenantId)
+//console.log(process.env.NODE_ENV)
+
+// export class ResourceSkusAPI {
+
+
+//   getProducts(filtersMap: Map<string, string>): Promise<ResourceSkus[]> {
+//     return getProducts(filtersMap);
+//   }
+// }
