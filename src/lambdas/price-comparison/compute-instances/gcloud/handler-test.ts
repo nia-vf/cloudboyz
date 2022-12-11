96daea070990b8d05e4be4111f89a3ddd261ae96
nia-vf <nia_vf@hotmail.com>
2022-12-11 13:41:10 +0000

    added new google package for compute, updated handler-test file to retrieve machine type

--- src/lambdas/price-comparison/compute-instances/gcloud/handler-test.ts
+++ src/lambdas/price-comparison/compute-instances/gcloud/handler-test.ts
@@ -1,7 +1,21 @@
 import { CloudCatalogClient } from "@google-cloud/billing";
-import { google } from "@google-cloud/billing/build/protos/protos";
+import { google as GoogleBilling } from "@google-cloud/billing/build/protos/protos";
+import { google as GoogleCompute } from "@google-cloud/compute/build/protos/protos";
+import { MachineTypesClient } from "@google-cloud/compute";
 import _ from "lodash";
 import * as apiResponse from "../../../../../data/gcloud/gcloud-compute-instances-pricing.json";
+import { machine } from "os";
+
+//Lambda request parameters
+interface Event {
+  region: string;
+  vcpus: string;
+  memory: string;
+  instanceFamily?: string;
+  tenancy?: string;
+  capacityStatus?: string;
+  preInstalledSoftware?: string;
+}
 
 //Lambda response body
 interface Cost {
@@ -24,24 +38,63 @@ interface Response {
   costs?: Cost[];
 }
 
-let catalogClient = new CloudCatalogClient();
-
-//var request: google.cloud.billing.v1.IListSkusRequest | undefined =
-
-//let request1 = new google.cloud.billing.v1.ListSkusRequest();
-
-const request: google.cloud.billing.v1.IListSkusRequest = {
-  parent: "services/6F81-5844-456A",
-  pageSize: 5000,
+//Dummy Event
+let dummyEvent: Event = {
+  region: "europe-west2",
+  vcpus: "4",
+  memory: "16",
 };
 
-async function callListSkus(request: google.cloud.billing.v1.IListSkusRequest) {
+//Get list of Machine Types
+async function callListMachineTypes(event: Event) {
+  //Create Client for Machine Types API
+  let machineTypesClient = new MachineTypesClient();
+
+  //Create Request Parameters
+  const machineTypeRequest: GoogleCompute.cloud.compute.v1.IListMachineTypesRequest =
+    {
+      project: "kubernetes-project-313908",
+      zone: "us-central1-a",
+      filter:
+        'description = "' +
+        event.vcpus +
+        " vCPUs, " +
+        event.memory +
+        ' GB RAM"',
+    };
+
+  var machineTypeList: GoogleCompute.cloud.compute.v1.IMachineType[] = [];
+
+  let machineTypeObject = await machineTypesClient.listAsync(
+    machineTypeRequest
+  );
+  let i = 0;
+  for await (const response of machineTypeObject) {
+    //console.log(response);
+    console.log(i);
+    i++;
+    machineTypeList.push(response);
+  }
+  return machineTypeList;
+}
+
+//Get List of Sku Prices
+async function callListSkus(event: Event) {
+  //Create Client for Pricing API
+  let catalogClient = new CloudCatalogClient();
+
+  //Create Request Parameters
+  const skuPriceRequest: GoogleBilling.cloud.billing.v1.IListSkusRequest = {
+    parent: "services/6F81-5844-456A",
+    pageSize: 5000,
+  };
+
   var skusList: any = [];
 
   if (apiResponse) {
     skusList = apiResponse;
   } else {
-    let iterable = await catalogClient.listSkusAsync(request, {
+    let iterable = await catalogClient.listSkusAsync(skuPriceRequest, {
       autoPaginate: false,
     });
 
@@ -55,10 +108,24 @@ async function callListSkus(request: google.cloud.billing.v1.IListSkusRequest) {
     }
   }
 
-  return skusList;
+  //Filter Skus for only requested region
+  var filteredSkus = _.filter(skusList, function (sku) {
+    return (
+      sku.category?.resourceFamily == "Compute" &&
+      //sku.category?.usageType == "OnDemand" &&
+      _.includes(sku.serviceRegions, event.region)
+    );
+  });
+
+  return filteredSkus;
 }
 
-function truncateSkusList(skusList: google.cloud.billing.v1.ISku[]) {
+//Truncate Sku Data
+function truncateSkusList(
+  event: Event,
+  machineTypes: GoogleCompute.cloud.compute.v1.IMachineType[],
+  skusList: GoogleBilling.cloud.billing.v1.ISku[]
+) {
   var filteredSkus = _.filter(skusList, function (sku) {
     return (
       sku.category?.resourceFamily == "Compute" &&
@@ -68,12 +135,15 @@ function truncateSkusList(skusList: google.cloud.billing.v1.ISku[]) {
     );
   });
 
+  console.log(filteredSkus);
   console.log(_.size(filteredSkus));
 }
 
 async function handlerExample() {
-  var skusList = await callListSkus(request);
-  truncateSkusList(skusList);
+  var machineTypeResponse = await callListMachineTypes(dummyEvent);
+  console.log(machineTypeResponse);
+  //var skusList = await callListSkus(dummyEvent);
+  //truncateSkusList(dummyEvent, machineTypeResponse, skusList);
 }
 
 handlerExample();
